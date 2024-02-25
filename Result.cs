9bdf38cc400da942f679d7d83d5a854b17a78b1b
gayemce <100995170+gayemce@users.noreply.github.com>
2024-02-25 22:10:23 +0300

    First Commit

---
+++ Result.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+
+namespace Gayemce.Result;
+public sealed class Result<T>
+{
+    public T? Data { get; set; }
+    public List<string>? ErrorMessages { get; set; }
+    public string? ErrorMessage { get; set; }
+    public bool IsSuccesful { get; set; } = true;
+    public int StatusCode { get; set; } = (int)HttpStatusCode.OK;
+
+    public Result(T data)
+    {
+        Data = data;
+    }
+
+    public Result(int statusCode, List<string> errorMessages)
+    {
+        IsSuccesful = false;
+        StatusCode = statusCode;
+        ErrorMessages = errorMessages;
+    }
+
+    public Result(int statusCode, string errorMessage)
+    {
+        IsSuccesful = false;
+        StatusCode = statusCode;
+        ErrorMessage = errorMessage;
+    }
+
+    public static implicit operator Result<T>(T data)
+    {
+        return new(data);
+    }
+
+    public static implicit operator Result<T>((int statusCode, List<string> errorMessages) parameters)
+    {
+        return new(parameters.statusCode, parameters.errorMessages);
+    }
+
+    public static implicit operator Result<T>((int statusCode, string errorMessage) parameters)
+    {
+        return new(parameters.statusCode, parameters.errorMessage);
+    }
+
+}
+
